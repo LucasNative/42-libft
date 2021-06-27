2177bc7932a33f6dfb563ce1f96559b316363daf
lucasnative <santos.lucasdanielsp@gmail.com>
2021-06-27 20:15:20 -0300

    code review itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ldaniel- <ldaniel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 21:08:27 by ldaniel-          #+#    #+#             */
-/*   Updated: 2021/06/24 19:52:05 by ldaniel-         ###   ########.fr       */
+/*   Updated: 2021/06/27 19:58:30 by ldaniel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,6 @@ static size_t	int_len(int i)
 	return (len);
 }
 
-static char	*negative(size_t n)
-{
-	char	*negative_str;
-	size_t	size;
-
-	size = int_len(n);
-	negative_str = ft_calloc((size + 2), sizeof(char));
-	if (negative_str == NULL)
-		return (NULL);
-	negative_str[0] = '-';
-	n = n * -1;
-	while (n != 0)
-	{
-		negative_str[--size + 1] = (n % 10) + '0';
-		n = n / 10;
-	}
-	return (negative_str);
-}
-
 char	*ft_itoa(int n)
 {
 	char	*str;
@@ -54,7 +35,9 @@ char	*ft_itoa(int n)
 	size = int_len(n);
 	if (n < 0)
 	{
-		return (negative(n));
+		n = n * -1;
+		size = size + 1;
+		str[0] = '-';
 	}
 	str = ft_calloc(size + 1, sizeof(char));
 	if (str == NULL)
