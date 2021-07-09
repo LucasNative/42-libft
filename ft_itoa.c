ab37e28bfc78286bb8ccb7ee98c8b23920cd0024
lucasnative <santos.lucasdanielsp@gmail.com>
2021-07-09 18:19:54 -0300

    code review

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ldaniel- <ldaniel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 21:08:27 by ldaniel-          #+#    #+#             */
-/*   Updated: 2021/06/27 21:14:57 by ldaniel-         ###   ########.fr       */
+/*   Updated: 2021/06/24 19:52:05 by ldaniel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,38 @@ static size_t	int_len(int i)
 	return (len);
 }
 
+static char	*negative(size_t n)
+{
+	char	*negative_str;
+	size_t	size;
+
+	size = int_len(n);
+	negative_str = ft_calloc((size + 2), sizeof(char));
+	if (negative_str == NULL)
+		return (NULL);
+	negative_str[0] = '-';
+	n = n * -1;
+	while (n != 0)
+	{
+		negative_str[--size + 1] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (negative_str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	size_t	size;
-	size_t	bign;
-	int		signal;
 
-	signal = 0;
-	bign = n;
 	size = int_len(n);
 	if (n < 0)
 	{
-		n = n * -1;
-		size = size + 1;
-		signal = 1;
+		return (negative(n));
 	}
 	str = ft_calloc(size + 1, sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	if (signal == 1)
-		str[0] = '-';
 	if (n == 0)
 		str[--size] = (n % 10) + '0';
 	while (n != 0)
